377d44b09b311f4165633f3376d7c3192c89857c
0J3lack0 <g.caglar.c@gmail.com>
2022-11-24 19:58:11 +0300

    test

---
+++ src/index.js
@@ -0,0 +1,17 @@
+//first webpack test
+import "./raheem.js"
+
+import dayjs from "dayjs"
+// console.log();
+document.getElementById("btn-1").addEventListener("click", btn_procedure);
+function btn_procedure(){
+  const btn = document.getElementById("btn-1");
+  alert ("asdljasdjsad");
+  btn.innerHTML = 'basildi'
+}
+
+
+console.log(dayjs().format("YY-MM-DD"));
+console.log("raheem");
+console.log("raheem");
+console.log("raheem");
